87d36e368521190779bb762866e3ad684a454d7e
Rezdev49 <donstayway514@gmail.com>
2023-03-16 08:37:28 +0700

    first commit

---
+++ plugins/creator.js
@@ -0,0 +1,3 @@
+let handler = async (m) => m.reply('Owner: wa.me/6283811034750') 
+handler.command = /^(owner)$/i
+module.exports = handler
---
+++ plugins/sc.js
@@ -0,0 +1,3 @@
+let handler = async (m) => m.reply('https://github.com/Rezdev49/Rez-bot') 
+handler.command = /^(sc)$/i
+module.exports = handler
